75008dbffd156a898bb096ae3d9f6eaed429caad
Justas <justas.valutis@student.vives.be>
2023-03-25 17:44:14 +0100

    Labo 6 update

--- 2-Semester/JS/Labo6/color-picker-uitbreiding/scripts/code.js
+++ 2-Semester/JS/Labo6/color-picker-uitbreiding/scripts/code.js
@@ -19,18 +19,26 @@ const update = () => {
     swatch.style.backgroundColor="rgb("+red+","+green+","+blue+")";
 }
 
-const save = () => {
-    console.log("Werkt")
+const save = swatch => {
+    //takes swatch element values
     let red=document.getElementById('sldRed').value;
     let green=document.getElementById('sldGreen').value;
     let blue=document.getElementById('sldBlue').value;
 
+    //creates swatch elemenets, by a click
     let sectionSwatches = document.getElementById('swatchComponents');
     let spanSwatches = document.createElement('span');
     spanSwatches.setAttribute('class', 'newSwatch');
     spanSwatches.style.backgroundColor="rgb("+red+","+green+","+blue+")";
     spanSwatches.addEventListener('click', updateMainSwatch);
 
+
+    //saves swatch element values to use on another function (updateMainSwatch)
+    spanSwatches.setAttribute('data-red', red);
+    spanSwatches.setAttribute('data-green', green);
+    spanSwatches.setAttribute('data-blue', blue);
+
+    //to remove swatch element by a click
     let remove = document.createElement('input');
     remove.setAttribute('class', 'remove')
     remove.type='button';
@@ -38,26 +46,27 @@ const save = () => {
     remove.addEventListener('click', removeSwatch);
     spanSwatches.appendChild(remove);
     sectionSwatches.appendChild(spanSwatches);
+
+
 }
 
 const removeSwatch = remove => {
     remove.target.parentElement.remove();
 }
 
-const updateMainSwatch = updateSwatch => {
-    const red = updateSwatch.target.getElementById('sldRed').value;
-    const green = updateSwatch.target.getElementById('sldGreen').value;
-    const blue = updateSwatch.target.getElementById('blue').value;
+const updateMainSwatch = event => {
+    //getting the values of clicked swatch element
+    const red = event.currentTarget.getAttribute('data-red');
+    const green = event.currentTarget.getAttribute('data-green');
+    const blue = event.currentTarget.getAttribute('data-blue');
 
-    document.getElementById('lblRed').innerHTML=red;
-    document.getElementById('lblGreen').innerHTML=green;
-    document.getElementById('lblBlue').innerHTML=blue;
+    //giving the slider values of clicked swatch elelement
+    document.getElementById('sldRed').value = red
+    document.getElementById('sldBlue').value = blue;
+    document.getElementById('sldGreen').value = green;
+    update();
 
-    let swatch = document.getElementById('swatch');
-    swatch.style.backgroundColor="rgb("+red+","+green+","+blue+")";
 
 }
 
 window.addEventListener("load", setup);
-
-// opletten by .value en .innerhtml
